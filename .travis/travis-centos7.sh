ca613cef0c0328fb5ef681585988a716cab23f5f
Stephen Gallagher <sgallagh@redhat.com>
2019-04-03 14:19:47 -0400

    CI: Rename centos to centos7
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- .travis/travis-centos7.sh
+++ .travis/travis-centos7.sh
@@ -24,18 +24,18 @@ repository="registry.centos.org"
 os="centos"
 
 sed -e "s/@IMAGE@/$repository\/$os:$release/" \
-    $SCRIPT_DIR/centos/Dockerfile.deps.tmpl > $SCRIPT_DIR/centos/Dockerfile.deps.$release
-sed -e "s/@RELEASE@/$release/" $SCRIPT_DIR/centos/Dockerfile.tmpl > $SCRIPT_DIR/centos/Dockerfile-$release
+    $SCRIPT_DIR/centos7/Dockerfile.deps.tmpl > $SCRIPT_DIR/centos7/Dockerfile.deps.$release
+sed -e "s/@RELEASE@/$release/" $SCRIPT_DIR/centos7/Dockerfile.tmpl > $SCRIPT_DIR/centos7/Dockerfile-$release
 
-sudo docker build -f $SCRIPT_DIR/centos/Dockerfile.deps.$release -t fedora-modularity/libmodulemd-deps-$release .
-sudo docker build -f $SCRIPT_DIR/centos/Dockerfile-$release -t fedora-modularity/libmodulemd:$release --build-arg TARBALL=$TARBALL .
+sudo docker build -f $SCRIPT_DIR/centos7/Dockerfile.deps.$release -t fedora-modularity/libmodulemd-deps-$release .
+sudo docker build -f $SCRIPT_DIR/centos7/Dockerfile-$release -t fedora-modularity/libmodulemd:$release --build-arg TARBALL=$TARBALL .
 
 if [ $release != "rawhide" ]; then
   # Only run Coverity scan on Rawhide since we have a limited number of scans per week.
   unset COVERITY_SCAN_TOKEN
 fi
 
-rm -f $TARBALL_PATH $SCRIPT_DIR/centos/Dockerfile.deps.$release $SCRIPT_DIR/centos/Dockerfile-$release
+rm -f $TARBALL_PATH $SCRIPT_DIR/centos7/Dockerfile.deps.$release $SCRIPT_DIR/centos7/Dockerfile-$release
 
 docker run -e COVERITY_SCAN_TOKEN=$COVERITY_SCAN_TOKEN -e TRAVIS=$TRAVIS -eTRAVIS_JOB_NAME="$TRAVIS_JOB_NAME" --rm fedora-modularity/libmodulemd:$release
 
