754ed250fc10b8fd13a2da2937b582d8e20270b7
Stephen Gallagher <sgallagh@redhat.com>
2021-01-05 09:19:50 -0500

    Packager: Skip override tests when the override is not in place
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- modulemd/tests/ModulemdTests/common.py
+++ modulemd/tests/ModulemdTests/common.py
@@ -22,86 +22,104 @@ logging.basicConfig(level=logging.DEBUG)
 
 class TestCommon(TestBase):
     def test_packager_read_file(self):
-        # Validate that the PackagerV2 specification parses correctly
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_packager_v2.yaml"
-            ),
-        )
-        self.assertIsNotNone(doc)
-        # Once read in, a modulemd-packager v2 document is a ModuleStream of the
-        # same version.
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names are undefined
-        self.assertIsNone(doc.get_module_name())
-        self.assertIsNone(doc.get_stream_name())
+        # We have a chicken-egg problem with overrides, since they can only
+        # be tested if they are already installed. This means they need to
+        # be run in the CI. In order to avoid changes to these tests or the
+        # overrides breaking things, we'll skip them if the appropriate
+        # override is not installed.
+        if "_overrides_module" in dir(Modulemd) and hasattr(
+            gi.overrides.Modulemd, "read_packager_file"
+        ):
 
-        # Read the PackagerV2 specification with module/stream name overrides
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_packager_v2.yaml"
-            ),
-            "modulename-override",
-            "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Validate that the PackagerV2 specification parses correctly
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_packager_v2.yaml"
+                ),
+            )
+            self.assertIsNotNone(doc)
+            # Once read in, a modulemd-packager v2 document is a ModuleStream of the
+            # same version.
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names are undefined
+            self.assertIsNone(doc.get_module_name())
+            self.assertIsNone(doc.get_stream_name())
 
-        # Valid PackagerV3 file
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_packager_v3.yaml"
-            ),
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.PackagerV3)
+            # Read the PackagerV2 specification with module/stream name overrides
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_packager_v2.yaml"
+                ),
+                "modulename-override",
+                "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
-        # Read PackagerV3 with module/stream name overrides
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_packager_v3.yaml"
-            ),
-            "modulename-override",
-            "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.PackagerV3)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Valid PackagerV3 file
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_packager_v3.yaml"
+                ),
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.PackagerV3)
 
-        # Validate that the ModuleStreamV2 specification parses correctly
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_stream_v2.yaml"
-            ),
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names are correct
-        self.assertEqual(doc.get_module_name(), "foo")
-        self.assertEqual(doc.get_stream_name(), "latest")
+            # Read PackagerV3 with module/stream name overrides
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_packager_v3.yaml"
+                ),
+                "modulename-override",
+                "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.PackagerV3)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
-        # Read the ModuleStreamV2 specification with module/stream name overrides
-        doc = Modulemd.read_packager_file(
-            os.path.join(
-                self.source_root, "yaml_specs/modulemd_stream_v2.yaml"
-            ),
-            "modulename-override",
-            "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Validate that the ModuleStreamV2 specification parses correctly
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_stream_v2.yaml"
+                ),
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names are correct
+            self.assertEqual(doc.get_module_name(), "foo")
+            self.assertEqual(doc.get_stream_name(), "latest")
+
+            # Read the ModuleStreamV2 specification with module/stream name overrides
+            doc = Modulemd.read_packager_file(
+                os.path.join(
+                    self.source_root, "yaml_specs/modulemd_stream_v2.yaml"
+                ),
+                "modulename-override",
+                "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
     def test_packager_read_string(self):
-        # PackagerV2
-        minimal_valid = """---
+        # We have a chicken-egg problem with overrides, since they can only
+        # be tested if they are already installed. This means they need to
+        # be run in the CI. In order to avoid changes to these tests or the
+        # overrides breaking things, we'll skip them if the appropriate
+        # override is not installed.
+        if "_overrides_module" in dir(Modulemd) and hasattr(
+            gi.overrides.Modulemd, "read_packager_string"
+        ):
+
+            # PackagerV2
+            minimal_valid = """---
 document: modulemd-packager
 version: 2
 data:
@@ -112,27 +130,27 @@ data:
       - MIT
 ...
 """
-        doc = Modulemd.read_packager_string(minimal_valid)
-        self.assertIsNotNone(doc)
-        # Once read in, a modulemd-packager document is a ModuleStream of the
-        # same version.
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names are undefined
-        self.assertIsNone(doc.get_module_name())
-        self.assertIsNone(doc.get_stream_name())
+            doc = Modulemd.read_packager_string(minimal_valid)
+            self.assertIsNotNone(doc)
+            # Once read in, a modulemd-packager document is a ModuleStream of the
+            # same version.
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names are undefined
+            self.assertIsNone(doc.get_module_name())
+            self.assertIsNone(doc.get_stream_name())
 
-        # Read again with module/stream name overrides
-        doc = Modulemd.read_packager_string(
-            minimal_valid, "modulename-override", "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Read again with module/stream name overrides
+            doc = Modulemd.read_packager_string(
+                minimal_valid, "modulename-override", "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
-        # PackagerV3
-        minimal_valid = """---
+            # PackagerV3
+            minimal_valid = """---
 document: modulemd-packager
 version: 3
 data:
@@ -142,25 +160,25 @@ data:
     - MIT
 ...
 """
-        doc = Modulemd.read_packager_string(minimal_valid)
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.PackagerV3)
-        # Confirm module and stream names are undefined
-        self.assertIsNone(doc.get_module_name())
-        self.assertIsNone(doc.get_stream_name())
+            doc = Modulemd.read_packager_string(minimal_valid)
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.PackagerV3)
+            # Confirm module and stream names are undefined
+            self.assertIsNone(doc.get_module_name())
+            self.assertIsNone(doc.get_stream_name())
 
-        # Read again with module/stream name overrides
-        doc = Modulemd.read_packager_string(
-            minimal_valid, "modulename-override", "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.PackagerV3)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Read again with module/stream name overrides
+            doc = Modulemd.read_packager_string(
+                minimal_valid, "modulename-override", "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.PackagerV3)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
-        # ModuleStreamV2
-        minimal_valid = """---
+            # ModuleStreamV2
+            minimal_valid = """---
 document: modulemd-stream
 version: 2
 data:
@@ -171,22 +189,22 @@ data:
       - MIT
 ...
 """
-        doc = Modulemd.read_packager_string(minimal_valid)
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names are undefined
-        self.assertIsNone(doc.get_module_name())
-        self.assertIsNone(doc.get_stream_name())
+            doc = Modulemd.read_packager_string(minimal_valid)
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names are undefined
+            self.assertIsNone(doc.get_module_name())
+            self.assertIsNone(doc.get_stream_name())
 
-        # Read again with module/stream name overrides
-        doc = Modulemd.read_packager_string(
-            minimal_valid, "modulename-override", "streamname-override",
-        )
-        self.assertIsNotNone(doc)
-        self.assertIs(type(doc), Modulemd.ModuleStreamV2)
-        # Confirm module and stream names were set
-        self.assertEqual(doc.get_module_name(), "modulename-override")
-        self.assertEqual(doc.get_stream_name(), "streamname-override")
+            # Read again with module/stream name overrides
+            doc = Modulemd.read_packager_string(
+                minimal_valid, "modulename-override", "streamname-override",
+            )
+            self.assertIsNotNone(doc)
+            self.assertIs(type(doc), Modulemd.ModuleStreamV2)
+            # Confirm module and stream names were set
+            self.assertEqual(doc.get_module_name(), "modulename-override")
+            self.assertEqual(doc.get_stream_name(), "streamname-override")
 
 
 if __name__ == "__main__":
